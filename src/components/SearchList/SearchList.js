dcb337d614caa896efa82bdc0ddd3dc320a14076
Andy <andy.egyud@gmail.com>
2019-04-24 09:25:26 -0700

    First letter in each name is now capitalied in search results

--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -5,7 +5,7 @@ import classes from './SearchList.module.css';
 const SearchList = ({pokemon}) => (
     <ul className={classes.SearchList}>
         {pokemon.map(poke => {
-          poke.name = poke.name.charAt(0).toUpperCase();
+          poke.name = poke.name.charAt(0).toUpperCase() + poke.name.slice(1);
           return <SearchListItem key={Math.random()}>{poke.name}</SearchListItem>
         })}
     </ul>
