9c1f710852a931fbf58491a9f07c45b0718ab4dd
Andy <andy.egyud@gmail.com>
2019-04-24 09:21:15 -0700

    Attempt to capitalize first letters

--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -3,11 +3,12 @@ import SearchListItem from './SearchListItem';
 import classes from './SearchList.module.css';
 
 const SearchList = ({pokemon}) => (
-  <ul className={classes.SearchList}>
-    {pokemon.map(poke => (
-      <SearchListItem key={Math.random()}>{poke.name}</SearchListItem>
-    ))}
-  </ul>
+    <ul className={classes.SearchList}>
+        {pokemon.map(poke => {
+          poke.name = poke.name.charAt(0).toUpperCase();
+          return <SearchListItem key={Math.random()}>{poke.name}</SearchListItem>
+        })}
+    </ul>
 )
 
 export default SearchList;
