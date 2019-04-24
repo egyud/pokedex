bd7f80e1726c77701f6b7091d22f992403df16cd
Andy <andy.egyud@gmail.com>
2019-04-24 11:30:01 -0700

    Added Links for detail page to wrap SearchListItem

--- src/components/SearchPage/SearchList/SearchList.js
+++ src/components/SearchPage/SearchList/SearchList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SearchListItem from './SearchListItem';
 import classes from './SearchList.module.css';
 
@@ -6,7 +7,11 @@ const SearchList = ({pokemon}) => (
     <ul className={classes.SearchList}>
         {pokemon.map(poke => {
           poke.name = poke.name.charAt(0).toUpperCase() + poke.name.slice(1);
-          return <SearchListItem key={Math.random()}>{poke.name}</SearchListItem>
+          return (
+            <Link to={"/details" + poke.name}>
+                <SearchListItem key={Math.random()}>{poke.name}</SearchListItem>  
+            </Link>
+          )
         })}
     </ul>
 )
--- src/components/SearchPage/SearchList/SearchListItem.js
+++ src/components/SearchPage/SearchList/SearchListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './SearchListItem.module.css';
 
 const SearchListItem = (props) => (
-  <li className={classes.SearchListItem}>{props.children}</li>
+  <li className={classes.SearchListItem} id={props.children}>{props.children}</li>
 );
 
 export default SearchListItem;
