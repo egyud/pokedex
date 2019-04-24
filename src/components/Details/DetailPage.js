82d33b706e746a14675e0c9c8763fea2a25209b1
Andy <andy.egyud@gmail.com>
2019-04-24 13:37:59 -0700

    Added basic displaying the poke's data

--- src/components/Details/DetailPage.js
+++ src/components/Details/DetailPage.js
@@ -21,9 +21,61 @@ class DetailPage extends Component {
   }
 
   render() {
+    let poke = this.state.data;
+    let table = <p>Loading...</p>;
+    if (this.state.data.stats !== undefined) {
+      table = 
+        <>
+          <div>
+            {poke.types.map(type => {
+              return <span>{type.type.name}</span>
+            })}
+          </div>
+          <table>
+            <tbody>
+              <tr>
+                <td>HP</td>
+                <td>{poke.stats[5].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Attack</td>
+                <td>{poke.stats[4].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Defense</td>
+                <td>{poke.stats[3].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Speed</td>
+                <td>{poke.stats[0].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Special Attack</td>
+                <td>{poke.stats[2].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Special Defense</td>
+                <td>{poke.stats[1].base_stat}</td>
+              </tr>
+              <tr>
+                <td>Height</td>
+                <td>{poke.height}</td>
+              </tr>
+              <tr>
+                <td>Weight</td>
+                <td>{poke.weight}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+    }
     return (
       <>
-        <div>{this.state.pokemon}</div>
+        <h1>{this.state.pokemon}</h1>
+        <div>
+          
+        </div>
+        {table}
       </>
     )
   }
