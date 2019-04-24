258d4d73d4b3fce7684f843d3f9773e5b58072be
Andy <andy.egyud@gmail.com>
2019-04-24 15:03:35 -0700

    Initial styling for data table

--- src/components/Details/DetailsTable/DetailsTable.js
+++ src/components/Details/DetailsTable/DetailsTable.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import classes from './DetailsTable.module.css';
 
 const DetailsTable = ({pokeData}) => (
-  <table>
+  <table className={classes.DetailsTable}>
     <tbody>
       <tr>
-        <td>HP</td>
+        <td className={classes.Category}>HP</td>
         <td>{pokeData.stats[5].base_stat}</td>
       </tr>
       <tr>
-        <td>Attack</td>
+        <td className={classes.Category}>Attack</td>
         <td>{pokeData.stats[4].base_stat}</td>
       </tr>
       <tr>
-        <td>Defense</td>
+        <td className={classes.Category}>Defense</td>
         <td>{pokeData.stats[3].base_stat}</td>
       </tr>
       <tr>
-        <td>Speed</td>
+        <td className={classes.Category}>Speed</td>
         <td>{pokeData.stats[0].base_stat}</td>
       </tr>
       <tr>
-        <td>Special Attack</td>
+        <td className={classes.Category}>Special Attack</td>
         <td>{pokeData.stats[2].base_stat}</td>
       </tr>
       <tr>
-        <td>Special Defense</td>
+        <td className={classes.Category}>Special Defense</td>
         <td>{pokeData.stats[1].base_stat}</td>
       </tr>
       <tr>
-        <td>Height</td>
+        <td className={classes.Category}>Height</td>
         <td>{pokeData.height}</td>
       </tr>
       <tr>
-        <td>Weight</td>
+        <td className={classes.Category}>Weight</td>
         <td>{pokeData.weight}</td>
       </tr>
     </tbody>
