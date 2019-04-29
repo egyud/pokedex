4780811dbd796e5b9c6e1add7f86781ed392ef8c
Andy <andy.egyud@gmail.com>
2019-04-29 08:47:44 -0700

    Set up server and build for deploying to heroku

---
+++ build/precache-manifest.5d4e87a9b8d5da38d98f3e41512c716a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9684aa8ba0fe1e8b6c4c1e6ed16ac7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b37d6551ac77c4c3d87",
+    "url": "/static/css/main.f2f06896.chunk.css"
+  },
+  {
+    "revision": "cd3a2230d3ef8688f333",
+    "url": "/static/js/2.d164fe64.chunk.js"
+  },
+  {
+    "revision": "2b37d6551ac77c4c3d87",
+    "url": "/static/js/main.263f86ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
